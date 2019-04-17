e26f967101913948020750aac16e8b367210f063
KotlyarovV <vkotl@yandex.ru>
2019-04-17 16:53:02 +0500

    forms

--- src/App.js
+++ src/App.js
@@ -6,27 +6,105 @@ class Position extends Component{
 
 }
 
-class Request extends Component {
-  render() {
-    return (
-        <div>
+
+class PaymentForm extends Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.number}
+                {this.props.applicant.name}
+                {this.props.type}
+            </div>
+        )
+    }
+}
+
+class CreatePaymentForm extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.createForm = this.createForm.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    createForm() {
+
+        fetch('https://localhost:44379/paymentForm',
+            {
+                method : 'post',
+                body : JSON.stringify(this.state.form)
+            })
+            .then(function (response) {
+                console.log();
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" onClick={this.createForm()}/>
+            </div>
+        )
+    }
+}
+
+class PaymentFormList extends Component{
+    constructor(props) {
+        super(props);
+        this.state = { forms : []};
+    }
+    fetchForms(){
+        fetch('https://localhost:44379/paymentForm')
+            .then(response => response.json())
+            .then(data =>
+                this.setState({
+                        forms : data
+                    }))
+            .catch(error => this.setState({error}));
+    }
+
+    componentDidMount() {
+        this.fetchForms();
+    }
+
+    render() {
+
+        const forms = this.state.forms.map((form) => <PaymentForm
+            number={form.number}
+            type = {form.type}
+            applicant = {form.applicant}/>)
+        return (
           <div>
-            {this.props.number}
+              {forms}
+              {this.state.error}
           </div>
-          <div>
-            {this.props.date}
-          </div>
-          <div>
-            {this.props.fio}
-          </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 class App extends Component {
   render() {
+      /*
     return (
+
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -43,7 +121,11 @@ class App extends Component {
           </a>
         </header>
       </div>
-    );
+    );*/
+      return (
+          //<PaymentFormList/>
+          <CreatePaymentForm/>
+      );
   }
 }
 
